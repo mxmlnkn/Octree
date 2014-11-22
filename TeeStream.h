882cf7a9d8427ad6074b90bb73e6d9838fb4c615
mxmlnkn <s3495379@mailbox.tu-dresden.de>
2014-11-22 11:02:49 +0100

    Correct passing by referencegit add TeeStream.h !
    This actually broke my Octree::operator<<, because I didn't want to allow a copy constructor and forgot to put an assert(false) into that copy constructor. With gdb it was a matter of seconds to find this error :3

--- TeeStream.h
+++ TeeStream.h
@@ -6,13 +6,11 @@
 #include <sstream>
 #include <string>
 
-using namespace std;
-
 /* outstream similar to cout but outputs (also/only) into files (tout/terr) */
 class TeeStream{
     public:
         int verbosity;
-        ofstream fileStream;
+        std::ofstream fileStream;
         TeeStream( void ) { }
         TeeStream( int verbosity = 1 ) { 
             this->verbosity = verbosity;
@@ -20,35 +18,35 @@ class TeeStream{
         TeeStream( const char * filename ) {
             fileStream.open( filename, std::ofstream::out | std::ofstream::app );
         }
-        void Open( string filename, int rank = 0 ) {
+        void Open( std::string filename, int rank = 0 ) {
             /* Create Timestamp and rank strings for filenames */
             time_t t = time(0);   // get time now
             struct tm * now = localtime( &t );
-            stringstream timestamp;
+            std::stringstream timestamp;
             timestamp << 1900 + now->tm_year << "-" << 1 + now->tm_mon << "-"
                       << now->tm_mday << "_" << now->tm_hour << "-"
                       << now->tm_min << "_";
-            string timesr = timestamp.str();
-            stringstream ranksr; ranksr << rank;
+            std::string timesr = timestamp.str();
+            std::stringstream ranksr; ranksr << rank;
 
-            fileStream.open( timesr + filename + string("_rank_") 
-                             + ranksr.str() + string(".log"),
+            fileStream.open( timesr + filename + std::string("_rank_") 
+                             + ranksr.str() + std::string(".log"),
                              std::ofstream::out | std::ofstream::app );
         }
         ~TeeStream(void) {
             fileStream.close();
         }
-        template <class T> TeeStream& operator<< (T val) {
+        template <class T> TeeStream& operator<<(T & val) {
             fileStream << val;
             if ( this->verbosity == 1 )
-                cout << val;
+                std::cout << val;
             fileStream.flush();
             return *this;
         }
-        TeeStream& operator<< (ostream& (*pfun)(ostream&)) {
+        TeeStream& operator<< (std::ostream& (*pfun)(std::ostream&)) {
             pfun(fileStream);
             if ( this->verbosity == 1 )
-                pfun(cout);
+                pfun(std::cout);
             return *this;
         }
 };
