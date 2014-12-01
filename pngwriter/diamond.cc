be83b19430e607e4c0854ff123487658ae1ecd90
mxmlnkn <s3495379@mailbox.tu-dresden.de>
2014-12-01 14:17:23 +0100

    Added PNGWriter 0.55 by Paul Blackburn and Axel Huebl
    reduced it to a more managable size of files and makefiles
    To compile the original sources from the github repo, I did:
    
        src/makefile: change first line to "include ../make.include.linux"
        make.include.linux: uncomment "P_FREETYPE = 1"
    
    Resulting commands (deleted some unnecessary flags) :
        g++ -O3 -Wall -DNO_FREETYPE -c -o pngwriter.o pngwriter.cc
        rm libpngwriter.a
    	ar -r -v libpngwriter.a pngwriter.o
    	ranlib libpngwriter.a
        # show functions headers of that archive :3... very cool!
        # nm --print-armap --demangle libpngwriter.a
    
        cd ../examples
        g++ -O3 -Wall -DNO_FREETYPE pngtest.cc -o pngtest -I../src -L../src
    -lz -lpngwriter -lpng
        ./pngtest.exe

---
+++ pngwriter/diamond.cc
@@ -0,0 +1,24 @@
+/** Copyright 2011      Kevin Ryde
+  * Copyright 2013-2014 Axel Huebl
+  *
+  * Example and bug report from Kevin Ryde
+  *
+  * License: GPLv2+
+  *
+  * http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=633405
+  */
+
+#include <pngwriter.h>
+#include <cstdlib>
+
+int main( )
+{
+  pngwriter img( 40,11, 0, "/tmp/foo.png" );
+
+  img.diamond      ( 10,6, 8,8,  1.0,1.0,1.0 );
+  img.filleddiamond( 30,6, 8,8,  1.0,1.0,1.0 );
+  img.write_png();
+
+  system( "convert /tmp/foo.png /tmp/foo.xpm; cat /tmp/foo.xpm" );
+  return 0;
+}
